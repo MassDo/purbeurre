3beec1f28a411e4388c5e0e6496ff92b2abaa923
MassDo <dorian_massoulier@hotmail.com>
2019-11-29 20:17:43 +0100

    connection front-back in navbar icon account/logout

--- purbeurre/account/views.py
+++ purbeurre/account/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
 def account(request):
     """
         view of the user account
     """
-    return render(request, 'account/account.html')
+
+    username = request.user.username
+    email = request.user.email
+
+    context = dict(
+        username = username,
+        email = email
+    )
+
+    return render(request, 'account/account.html', context)
--- purbeurre/core/urls.py
+++ purbeurre/core/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('legals/', include('legals.urls')),
 
-    path('signup/', signup_views.signup, name='signup-signup'),
+    path('signup/', signup_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', authentication_form=UserLoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
