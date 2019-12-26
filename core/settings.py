6ef78cb80e917d8689dd527b661cfa03a992c0b7
MassDo <dorian_massoulier@hotmail.com>
2019-12-26 14:33:54 +0100

    debug false, hide secret key

--- core/settings.py
+++ core/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2@rcd^y!#ecnpwo!!7=p#-xgy!_vdqyl41_@db$s7ms2qq5ch1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
